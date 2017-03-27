e1d986fae7929c49216285f07990b7e8ce8d51cf
Sweta <Swetagsanghavi@gmail.com>
2017-03-26 22:53:21 -0700

    Add production webpack config and newrelic

--- frontend/components/app.jsx
+++ frontend/components/app.jsx
@@ -1,3 +1,4 @@
+require('newrelic');
 import React from 'react';
 import { Link } from 'react-router';
 import NavBarContainer from './nav_bar/nav_bar_container';
---
+++ newrelic.js
@@ -0,0 +1,26 @@
+'use strict'
+
+/**
+ * New Relic agent configuration.
+ *
+ * See lib/config.defaults.js in the agent distribution for a more complete
+ * description of configuration variables and their potential values.
+ */
+exports.config = {
+  /**
+   * Array of application names.
+   */
+  app_name: ['surf_dystopia'],
+  /**
+   * Your New Relic license key.
+   */
+  license_key: 'f9a27cc813e0cf24572bdfde542c93a870a6ff3b',
+  logging: {
+    /**
+     * Level at which to log. 'trace' is most useful to New Relic when diagnosing
+     * issues with the agent, 'info' and higher will impose the least overhead on
+     * production applications.
+     */
+    level: 'info'
+  }
+};
