b2f16bfad87a655f37207af2f75351e62a855d20
Sweta Sanghavi <swetagsanghavi@gmail.com>
2017-05-09 15:13:45 -0700

    Add validation tests for user model

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -2,4 +2,7 @@ require 'rails_helper'
 
 RSpec.describe User, type: :model do
    it { should validate_presence_of(:username) }
+   it { should validate_presence_of(:password_digest) }
+   it { should validate_presence_of(:session_token) }
+   it { should validate_presence_of(:password) }
 end
