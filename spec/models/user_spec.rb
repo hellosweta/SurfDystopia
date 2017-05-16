9dde8f55e5618ffe51043490ad41f2d9654a203d
App Academy Student <student@appacademy.io>
2017-05-15 22:03:09 -0700

    Add tests for #find_by_credentials

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,10 +1,10 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
+  before(:each) do
+    User.create!(username: 'hello_sweta2', name: 'Sweta', password: 'password')
+  end
   describe 'validations' do
-    before(:each) do
-      User.create!(username: 'hello_sweta2', name: 'Sweta', password: 'password')
-    end
     it { should validate_presence_of(:username) }
     it { should validate_uniqueness_of(:username) }
     it { should validate_presence_of(:name) }
@@ -17,6 +17,33 @@ RSpec.describe User, type: :model do
     it { should have_many(:listings) }
     it { should have_many(:reviews) }
     it { should have_many(:bookings) }
+  end
+
+  describe 'model_methods' do
+    describe '.find_by_credentials' do
+      context 'when given correct credentials' do
+        it 'should find the right user' do
+          correct_user = User.find_by_credentials('guest', 'password')
+          expect(correct_user).to eq(user)
+        end
+      end
+
+      context 'when given incorrect credentials' do
+        it 'should return nil' do
+          incorrect_user = User.find_by_credentials('guest', 'wrongpw')
+          expect(incorrect_user).to eq(nil)
+        end
+      end
+    end
+  end
+  describe 'instance_methods' do
+    describe '#reset_session_token!' do
+      it 'resets the session token' do
+        old_token = user.session_token
+        new_token = user.reset_session_token!
+        expect(old_token).not_to eq(new_token)
+      end
+    end
 
   end
 end
