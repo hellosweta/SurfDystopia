0088da352b2df4d7f9ad4f778922a1cbd25d921d
App Academy Student <student@appacademy.io>
2017-05-15 18:02:23 -0700

    Add user validation and association tests

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,8 +1,22 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-   it { should validate_presence_of(:username) }
-   it { should validate_presence_of(:password_digest) }
-   it { should validate_presence_of(:session_token) }
-   it { should validate_presence_of(:password) }
+  describe 'validations' do
+    before(:each) do
+      User.create!(username: 'hello_sweta2', name: 'Sweta', password: 'password')
+    end
+    it { should validate_presence_of(:username) }
+    it { should validate_uniqueness_of(:username) }
+    it { should validate_presence_of(:name) }
+    it { should validate_presence_of(:password_digest) }
+    it { should validate_presence_of(:session_token) }
+    it { should validate_length_of(:password).is_at_least(6) }
+  end
+
+  describe 'associations ' do
+    it { should have_many(:listings) }
+    it { should have_many(:reviews) }
+    it { should have_many(:bookings) }
+
+  end
 end
